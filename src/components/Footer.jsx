8103eafb172483cd4aa438fc5c71ddb305bc2611
Yogeshwar Suryavamshi <yogi.s75@gmail.com>
2022-07-14 17:31:20 +0530

    Footer and done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <img src="/src/icons/twit-icon.png" alt="" />
+      <img src="/src/icons/fb-icon.png" alt="" />
+      <img src="/src/icons/insta-icon.png" alt="" />
+      <img src="/src/icons/gh-icon.png" alt="" />
+    </div>
+  );
 }
 
 export default Footer;
