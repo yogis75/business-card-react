e1147609c8bf9bdc2d60862683bf6b9cbf194431
Yogeshwar Suryavamshi <yogi.s75@gmail.com>
2022-07-17 18:22:17 +0530

    links and some css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,18 @@ import React from "react";
 function Footer() {
   return (
     <div className="footer">
-      <img src="/src/icons/twit-icon.png" alt="" />
-      <img src="/src/icons/fb-icon.png" alt="" />
-      <img src="/src/icons/insta-icon.png" alt="" />
-      <img src="/src/icons/gh-icon.png" alt="" />
+      <a href="http://twitter.com">
+        <img src="/src/icons/twit-icon.png" alt="" />
+      </a>
+      <a href="http://fb.com/yogi.s75">
+        <img src="/src/icons/fb-icon.png" alt="" />
+      </a>
+      <a href="https://www.instagram.com/yogeshwersuryavanshi/">
+        <img src="/src/icons/insta-icon.png" alt="" />
+      </a>
+      <a href="http://github.com/yogis75">
+        <img src="/src/icons/gh-icon.png" alt="" />
+      </a>
     </div>
   );
 }
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -6,15 +6,21 @@ function Info() {
       <img className="info--img" src="/src/images/pic.jpg" alt="pic" />
       <h3 className="info--name">Yogeshwar S</h3>
       <p className="info--role">Frontend developer</p>
-      <p className="info--website">yogeshwar.dev</p>
+      <p className="info--website">
+        <a href="http://yogeshwar.dev">yogeshwar.dev</a>
+      </p>
       <div className="btns">
         <button className="info--btn_email">
-          <img src="/src/icons/mail.png" />
-          Email
+          <a href="mailto:yogi.s75@gmail.com">
+            <img src="/src/icons/mail.png" />
+            <p>Email</p>
+          </a>
         </button>
         <button className="info--btn_linkedin">
-          <img src="/src/icons/in.png" />
-          Linkedin
+          <a href="http://www.linkedin.com">
+            <img src="/src/icons/in.png" />
+            <p>Linkedin</p>
+          </a>
         </button>
       </div>
     </div>
